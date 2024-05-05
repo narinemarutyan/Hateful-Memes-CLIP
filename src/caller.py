ed645cf3e55d98da17af1f4b3887b17eec6e6768
narinemarutyan <narine_m@purlin.com>
2024-05-06 01:58:35 +0400

    write docstrings for caller.py

--- src/caller.py
+++ src/caller.py
@@ -1,15 +1,43 @@
+from typing import List, Dict, Any
+
 import torch
 from transformers import CLIPProcessor, CLIPTokenizer
 
 
 class Caller(object):
-
+    """
+    A class to preprocess images and texts using CLIP models for a given batch
+    """
     def __init__(self, args):
+        """
+        Initialize the Caller Class
+
+        Parameters
+        ----------
+        args : argparse.Namespace
+            Arguments passed to the Caller class
+
+        Returns
+        -------
+        out : None
+        """
         self.args = args
         self.image_processor = CLIPProcessor.from_pretrained(args.clip_pretrained_model)
         self.text_processor = CLIPTokenizer.from_pretrained(args.clip_pretrained_model)
 
-    def __call__(self, batch):
+    def __call__(self, batch: List[Dict[str, Any]]) -> Dict[str, torch.Tensor]:
+        """
+        Processes a batch of data dictionaries containing images, texts, captions, and labels
+
+        Parameters
+        ----------
+        batch : List[Dict[str, Any]]
+             A list of dictionaries each containing 'image', 'text', 'caption', and 'label'
+
+        Returns
+        -------
+        out : Dict[str, torch.Tensor]
+        """
         pixel_values = self.image_processor(images=[item['image'] for item in batch], return_tensors="pt")[
             'pixel_values']
 
