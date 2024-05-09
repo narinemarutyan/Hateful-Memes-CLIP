44c2a072d4b5ea7cb85a7431b05df8ad22cd6075
Narine Marutyan <89347637+narinemarutyan@users.noreply.github.com>
2024-05-09 17:21:34 +0400

    Add files via upload

---
+++ preprocess/blip.py
@@ -0,0 +1,55 @@
+import os
+import argparse
+from PIL import Image
+import pandas as pd
+from transformers import BlipProcessor, BlipForConditionalGeneration
+
+def generate_captions(json_dir, img_folder):
+    splits = ['train', 'dev', 'test']
+    df = []
+    for split_name in splits:
+        file_path = os.path.join(json_dir, f'{split_name}.json')
+        split_df = pd.read_json(file_path, lines=True)
+        split_df['split'] = split_name
+        df.append(split_df)
+
+    df = pd.concat(df, axis=0, ignore_index=True)
+    df['id'] = df['img'].str.split('/').str[1].str.split('.').str[0]
+    df.index = df['id']
+    df.index.name = None
+
+    processor = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
+    model = BlipForConditionalGeneration.from_pretrained("Salesforce/blip-image-captioning-base").to("cuda")
+
+    captions = []
+    for img_fn in df['img'].str.split('/').str[1]:
+        img_fp = os.path.join(img_folder, img_fn)
+        img = Image.open(img_fp).convert("RGB")
+        device = "cuda"
+        inputs = processor(images=img, return_tensors="pt").to(device)
+        pixel_values = inputs.pixel_values
+        generated_ids = model.generate(pixel_values=pixel_values, max_length=50)
+        generated_caption = processor.batch_decode(generated_ids, skip_special_tokens=True)[0]
+        captions.append(generated_caption)
+
+    df['caption'] = captions
+    float_cols = df.select_dtypes(include=[float]).columns
+    df[float_cols] = df.select_dtypes(include=[float]).astype('Int64')
+    df.to_csv("hateful_memes_expanded_blip.csv")
+
+def main():
+    parser = argparse.ArgumentParser(description="Generate captions for images in a dataset")
+    parser.add_argument("--json_dir", type=str, default="/path/to/json/directory",
+                        help="Directory where JSON files are stored")
+    parser.add_argument("--img_folder", type=str, default="/path/to/image/folder",
+                        help="Directory where images are stored")
+    args = parser.parse_args()
+
+    generate_captions(args.json_dir, args.img_folder)
+
+if __name__ == "__main__":
+    main()
+#python blip.py --json_dir /path/to/json --img_folder /path/to/image/folder
+
+
+
